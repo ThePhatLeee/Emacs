d02c16b0a06c8bf87afa6a2d791597871ca15a64
Marko Jokinen <thephatle@proton.com>
2026-01-09 00:08:55 -0700

    natural scroll and timezone and keyboard locale settings

--- profiles/workstation.nix
+++ profiles/workstation.nix
@@ -9,8 +9,8 @@
     ../modules/shared
   ];
 
-  time.timeZone = lib.mkDefault "America/Edmonton";
-  i18n.defaultLocale = "en_CA.UTF-8";
+  time.timeZone = lib.mkDefault "Europe/Helsinki";
+  i18n.defaultLocale = "en_GB.UTF-8";
   boot.loader.systemd-boot.configurationLimit = 20;
 
   # Optimizations
